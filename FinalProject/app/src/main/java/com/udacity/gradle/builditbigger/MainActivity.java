ffed50ad5a512102f12dd78cf50fdb8844452660
Jack Hayward <38814148+JackHayward@users.noreply.github.com>
2020-11-25 12:23:31 +0000

    Added joker to app

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
+import com.example.javajokes.Joker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+
+        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
     }
-
-
 }
